f24c9e46955a3003276e8487a8435ed401bb9b92
Fadi Ahmad <fadich95@gmail.com>
2023-12-23 13:31:52 +0200

    Lesson 11

---
+++ lesson_11/files.py
@@ -0,0 +1,22 @@
+
+line = "A quick brown fox jumps over the lazy dog"
+
+open("..\my_file.txt", "b")
+
+
+def aaa(a, b, c):
+    pass
+
+
+a = {
+    "a": 1,
+    "b": 2
+
+}
+aaa(
+    a=1,
+    b=2,
+    c=3,
+)
+
+aaa(1, 2, 3)
