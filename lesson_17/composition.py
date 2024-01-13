4d2e7ffdaf9d2375d119fb1d36a2bd3d59391d68
Fadi Ahmad <fadich95@gmail.com>
2024-01-13 13:02:07 +0200

    Lesson 17

---
+++ lesson_17/composition.py
@@ -0,0 +1,70 @@
+from typing import (
+    List,
+    Optional,
+)
+
+
+class Furniture:
+    pass
+
+
+class Wall:
+    pass
+
+
+class Room:
+
+    def __init__(
+        self,
+        walls: List[Wall],
+        furniture: Optional[List[Furniture]] = None,
+    ):
+        self.walls = walls
+
+        if furniture is None:
+            furniture = []
+        self.furniture = furniture
+
+
+room = Room(
+    walls=[
+        Wall(),
+        Wall(),
+        Wall(),
+        Wall(),
+    ],
+)
+
+sofa = Furniture()
+table = Furniture()
+chair = Furniture()
+
+room.furniture.append(table)
+room.furniture.remove(table)
+
+del room
+
+###############################################################################
+
+
+class Character:
+
+    def eat(self):
+        ...
+
+
+class Robot:
+
+    def __init__(self):
+        self.c = Character()
+
+    def eat(self):
+        return self.c.eat()
+
+
+r = Robot()
+r.eat()
+r.c.eat()
+
+
+print(isinstance(r, Character))  # False
