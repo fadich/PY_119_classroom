a5744972468c7959b58fadad3cdc69a5098b6ac9
Fadi Ahmad <fadich95@gmail.com>
2024-01-24 21:11:43 +0200

    Lesson 22

--- lesson_22/convertor.py
+++ lesson_22/convertor.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any
 
 
@@ -19,6 +20,7 @@ class Convertor:
 
 
 class CelsiusToKelvinConvertor(Convertor):
+    TEMP_KEL = "K"
 
     @classmethod
     def get_unit_from_name(cls) -> str:
@@ -26,18 +28,75 @@ class CelsiusToKelvinConvertor(Convertor):
 
     @classmethod
     def get_unit_to_name(cls) -> str:
-        return "K"
+        return cls.TEMP_KEL
 
     def convert(self) -> Any:
         return float(self.value) + 273.15
 
 
+class KelvinToCelsiusConvertor(Convertor):
+    TEMP_KEL = "K"
+
+    @classmethod
+    def get_unit_from_name(cls) -> str:
+        return cls.TEMP_KEL
+
+    @classmethod
+    def get_unit_to_name(cls) -> str:
+        return "C"
+
+    def convert(self) -> Any:
+        return float(self.value) - 273.15
+
+
+class JsonObjToValuesConvertor(Convertor):
+
+    @classmethod
+    def get_unit_from_name(cls) -> str:
+        return "json object"
+
+    @classmethod
+    def get_unit_to_name(cls) -> str:
+        return "values"
+
+    def convert(self) -> Any:
+        return json.loads(self.value).values()
+
+
+class UAHToUSDConvertor(Convertor):
+
+    @classmethod
+    def get_unit_from_name(cls) -> str:
+        return "UAH"
+
+    @classmethod
+    def get_unit_to_name(cls) -> str:
+        return "USD"
+
+    def convert(self) -> Any:
+        return float(self.value) / 39.5
+
+
+def create_convertor(name: str) -> Convertor:
+    if name == "c2k":
+        return CelsiusToKelvinConvertor(value=value)
+    if name == "k2c":
+        return KelvinToCelsiusConvertor(value=value)
+    if name == "j2v":
+        return JsonObjToValuesConvertor(value=value)
+    if name == "u2s":
+        return UAHToUSDConvertor(value=value)
+
+    raise NotImplementedError(f"There is not such convertor <{name}>")
+
+
 if __name__ == "__main__":
-    c2k = CelsiusToKelvinConvertor(
-        value=input("Value: "),
-    )
+    name = input("Convertor name: ")
+    value = input("Value: ")
+
+    convertor: Convertor = create_convertor(name)
 
     print(
-        f"{c2k.value}{c2k.get_unit_from_name()} -> "
-        f"{c2k.convert()}{c2k.get_unit_to_name()}",
+        f"{convertor.value}{convertor.get_unit_from_name()} -> "
+        f"{convertor.convert()}{convertor.get_unit_to_name()}",
     )
