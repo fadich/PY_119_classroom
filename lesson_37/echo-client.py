b6628de32df5aa2d3bdb884e68e7fd18589f7482
Fadi Ahmad <fadich95@gmail.com>
2024-03-06 19:39:14 +0200

    Lesson 37

---
+++ lesson_37/echo-client.py
@@ -0,0 +1,14 @@
+import socket
+
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+
+with socket.socket() as s:
+    s.connect((HOST, PORT))
+    msg_bytes = input(">>> ").encode()
+    s.sendall(msg_bytes)
+    data = s.recv(1024)
+
+print('Received', repr(data))
---
+++ lesson_37/echo-server.py
@@ -0,0 +1,21 @@
+import socket
+
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+
+with socket.socket() as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+
+    conn, addr = s.accept()
+    with conn:
+        print("Connected by", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data)
+            conn.sendall(data)
+            break
